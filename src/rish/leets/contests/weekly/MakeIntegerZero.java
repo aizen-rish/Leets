be0430c302eef39dbba8236f1016bda31a72e0dd
rish <kjeremyth@yandex.com>
2023-07-05 00:37:48 +0530

    problem2 --unsolved

---
+++ src/rish/leets/contests/weekly/MakeIntegerZero.java
@@ -0,0 +1,25 @@
+package rish.leets.contests.weekly;
+
+/*
+ * 				Weekly Contest 351
+ * 
+ * Problem #: 2749
+ * Problem link: https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero/
+ * Contest link: https://leetcode.com/contest/weekly-contest-351/
+ *
+ * @author Rishabh Soni
+ * 
+ * Date Attempted: 28/06/2023
+ */
+public class MakeIntegerZero {
+
+    public int makeTheIntegerZero(int num1, int num2) {
+
+		if (num2 > num1) {
+            return -1;
+        }
+
+		return 0;
+    }
+
+}
