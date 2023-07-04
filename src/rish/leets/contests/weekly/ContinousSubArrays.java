65fe38340038c1ee996a90751ed2488d0567fd6e
rish <kjeremyth@yandex.com>
2023-07-05 00:26:00 +0530

    problem 3 --unsolved --draft

---
+++ src/rish/leets/contests/weekly/ContinousSubArrays.java
@@ -0,0 +1,21 @@
+package rish.leets.contests.weekly;
+
+/*
+ * 				Weekly Contest 352
+ * 
+ * Problem link: https://leetcode.com/problems/robot-collisions/
+ * 
+ * Attempt Date: 04/07/2023
+ */
+public class ContinousSubArrays {
+
+	public long continuousSubarrays(int[] nums) {
+		return 0L;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
