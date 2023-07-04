2ca24b71e50dd98c909fa33d655606d3043eaced
rish <kjeremyth@yandex.com>
2023-07-05 01:05:09 +0530

    problem3 --solved

--- src/rish/leets/contests/weekly/SplitIntoGoodArrays.java
+++ src/rish/leets/contests/weekly/SplitIntoGoodArrays.java
@@ -13,8 +13,36 @@ package rish.leets.contests.weekly;
  */
 public class SplitIntoGoodArrays {
 
-    public int numberOfGoodSubarraySplits(int[] nums) {
-        return 0;
-    }
+	public static int numberOfGoodSubarraySplits(int[] nums) {
+
+		long m = 1000000007;
+		long answer = 1;
+		int count = 0;
+
+		int i = 0;
+		while (i < nums.length && nums[i] == 0) {
+			i++;
+		}
+
+		if (i == nums.length) {
+			return 0;
+		}
+
+		for (int j = i; j < nums.length; j++) {
+			if (nums[j] == 0) {
+				count++;
+				continue;
+			}
+			answer = (answer * (count + 1)) % m;
+			count = 0;
+		}
+
+		return (int) answer;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 0, 0, 1, 1 };
+		System.out.println(numberOfGoodSubarraySplits(arr));
+	}
 
 }
