11442583daaf632fcd9f32ecf51b12b6aba2c58a
rish <kjeremyth@yandex.com>
2023-07-05 00:38:02 +0530

    problem3 --draft

---
+++ src/rish/leets/contests/weekly/SplitIntoGoodArrays.java
@@ -0,0 +1,20 @@
+package rish.leets.contests.weekly;
+
+/*
+ * 				Weekly Contest 351
+ * 
+ * Problem #: 2750
+ * Problem link: https://leetcode.com/problems/ways-to-split-array-into-good-subarrays
+ * Contest link: https://leetcode.com/contest/weekly-contest-351/
+ *
+ * @author Rishabh Soni
+ * 
+ * Date Attempted: n/a
+ */
+public class SplitIntoGoodArrays {
+
+    public int numberOfGoodSubarraySplits(int[] nums) {
+        return 0;
+    }
+
+}
