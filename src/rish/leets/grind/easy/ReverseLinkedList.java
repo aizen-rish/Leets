8c1263fb3ba82ddef60e9440f1bee9e4a0d986b9
rish <kjeremyth@yandex.com>
2023-07-06 01:09:49 +0530

    adds reverse linked list code --iterative --recursive

---
+++ src/rish/leets/grind/easy/ReverseLinkedList.java
@@ -0,0 +1,68 @@
+package rish.leets.grind.easy;
+
+/*
+ * Grind 75 : Week 2
+ * 
+ * Problem #: 206
+ * Problem link : https://leetcode.com/problems/reverse-linked-list/
+ * 
+ * @author Rishabh Soni
+ *
+ */
+public class ReverseLinkedList {
+
+		/*
+		 * Definition for singly-linked list.
+		 */
+		class ListNode {
+
+				int val;
+				ListNode next;
+
+				ListNode() {
+				}
+
+				ListNode(int val) {
+						this.val = val;
+				}
+
+				ListNode(int val, ListNode next) {
+						this.val = val;
+						this.next = next;
+				}
+
+		}
+
+		public ListNode reverseListRecursive(ListNode head) {
+
+				if (head == null || head.next == null) {
+						return head;
+				}
+
+				ListNode hNext = head.next;
+				head.next = null;
+				ListNode rHead = reverseListRecursive(hNext);
+				hNext.next = head;
+				return rHead;
+		}
+
+		public ListNode reverseListIterative(ListNode head) {
+
+				if (head == null || head.next == null) {
+						return head;
+				}
+
+				ListNode itr = head;
+				ListNode rHead = null;
+
+				while (itr != null) {
+						ListNode temp = new ListNode(itr.val);
+						temp.next = rHead;
+						rHead = temp;
+						itr = itr.next;
+				}
+
+				return rHead;
+		}
+
+}
