84862f9a90ccb598a57844f59ff9fb0a68ac80f7
rish <kjeremyth@yandex.com>
2023-07-06 01:09:56 +0530

    formatting changes

--- src/rish/leets/grind/easy/MiddleOfLinkedList.java
+++ src/rish/leets/grind/easy/MiddleOfLinkedList.java
@@ -11,53 +11,54 @@ package rish.leets.grind.easy;
  */
 public class MiddleOfLinkedList {
 
-	/*
-	 * Definition for singly-linked list.
-	 */
-	class ListNode {
-
-		int val;
-		ListNode next;
-
-		ListNode() {
-		}
-
-		ListNode(int val) {
-			this.val = val;
-		}
-
-		ListNode(int val, ListNode next) {
-			this.val = val;
-			this.next = next;
-		}
-
-	}
-
-	public ListNode middleNode(ListNode head) {
-
-		if (head.next == null) {
-			return head;
-		}
-
 		/*
-		 * Use two pointers approach Pointer i moves one node ahead at a time Pointer j
-		 * moves two nodes ahead at a time
+		 * Definition for singly-linked list.
 		 */
-		ListNode i = head;
-		ListNode j = head;
+		class ListNode {
 
-		do {
-			if (j.next == null) {
-				return i;
-			}
-			if (j.next.next == null) {
-				return i.next;
-			}
-			i = i.next;
-			j = j.next.next;
-		} while (i != null && j != null);
+				int val;
+				ListNode next;
 
-		return head;
-	}
+				ListNode() {
+				}
+
+				ListNode(int val) {
+						this.val = val;
+				}
+
+				ListNode(int val, ListNode next) {
+						this.val = val;
+						this.next = next;
+				}
+
+		}
+
+		public ListNode middleNode(ListNode head) {
+
+				if (head.next == null) {
+						return head;
+				}
+
+				/*
+				 * Use two pointers approach Pointer i moves one node ahead at a time Pointer j
+				 * moves two nodes ahead at a time
+				 */
+				ListNode i = head;
+				ListNode j = head;
+
+				do {
+						if (j.next == null) {
+								return i;
+						}
+						if (j.next.next == null) {
+								return i.next;
+						}
+						i = i.next;
+						j = j.next.next;
+				}
+				while (i != null && j != null);
+
+				return head;
+		}
 
 }
