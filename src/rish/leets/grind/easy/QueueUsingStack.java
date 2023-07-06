5e92cd47d2fa8b90d7fba377b0728bb0cd8cef2d
rish <kjeremyth@yandex.com>
2023-07-06 22:32:32 +0530

    test formatting change

--- src/rish/leets/grind/easy/QueueUsingStack.java
+++ src/rish/leets/grind/easy/QueueUsingStack.java
@@ -13,79 +13,79 @@ import java.util.Stack;
  */
 class MyQueue {
 
-	private Stack<Integer> s1;
-	private Stack<Integer> s2;
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
 
-	private boolean isOrdered;
+    private boolean isOrdered;
 
-	public MyQueue() {
-		this.s1 = new Stack<>();
-		this.s2 = new Stack<>();
-		this.isOrdered = false;
-	}
+    public MyQueue() {
+        this.s1 = new Stack<>();
+        this.s2 = new Stack<>();
+        this.isOrdered = false;
+    }
 
-	public void push(int x) {
+    public void push(int x) {
 
-		if (isOrdered) {
-			reverseFill(s2, s1);
-			isOrdered = false;
-		}
+        if (isOrdered) {
+            reverseFill(s2, s1);
+            isOrdered = false;
+        }
 
-		s1.push(x);
-	}
+        s1.push(x);
+    }
 
-	public int pop() {
+    public int pop() {
 
-		if (isOrdered) {
-			return s2.pop();
-		}
+        if (isOrdered) {
+            return s2.pop();
+        }
 
-		// If not ordered, reverse elements
-		reverseFill(s1, s2);
-		isOrdered = true;
-		return s2.pop();
-	}
+        // If not ordered, reverse elements
+        reverseFill(s1, s2);
+        isOrdered = true;
+        return s2.pop();
+    }
 
-	public int peek() {
+    public int peek() {
 
-		if (isOrdered) {
-			return s2.peek();
-		}
+        if (isOrdered) {
+            return s2.peek();
+        }
 
-		// If not ordered, reverse elements
-		reverseFill(s1, s2);
-		isOrdered = true;
-		return s2.peek();
-	}
+        // If not ordered, reverse elements
+        reverseFill(s1, s2);
+        isOrdered = true;
+        return s2.peek();
+    }
 
-	private void reverseFill(Stack<Integer> src, Stack<Integer> dest) {
+    private void reverseFill(Stack<Integer> src, Stack<Integer> dest) {
 
-		if (src.empty()) {
-			return;
-		}
+        if (src.empty()) {
+            return;
+        }
 
-		while (!src.empty()) {
-			dest.add(src.pop());
-		}
-	}
+        while (!src.empty()) {
+            dest.add(src.pop());
+        }
+    }
 
-	public boolean empty() {
-		return isOrdered ? s2.empty() : s1.empty();
-	}
+    public boolean empty() {
+        return isOrdered ? s2.empty() : s1.empty();
+    }
 
 }
 
 public class QueueUsingStack {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		MyQueue q = new MyQueue();
-		q.push(1);
-		q.push(2);
+        MyQueue q = new MyQueue();
+        q.push(1);
+        q.push(2);
 
-		System.out.println(q.peek());
-		System.out.println(q.pop());
-		System.out.println(q.pop());
-	}
+        System.out.println(q.peek());
+        System.out.println(q.pop());
+        System.out.println(q.pop());
+    }
 
 }
