5563b383e317c268237cbbe55d3ba55f8c2c953c
rish <kjeremyth@yandex.com>
2023-07-13 19:40:19 +0530

    Grind-75: Week #2 solutions (#2)

---
+++ src/rish/leets/grind/medium/LowestCommonAncestor.java
@@ -0,0 +1,44 @@
+package rish.leets.grind.medium;
+
+/*
+ * Grind 75 : Week 2
+ * 
+ * Problem #: 235
+ * Problem link : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * 
+ * Date Attempted: 05/07/2023
+ * 
+ * @author Rishabh Soni
+ *
+ */
+public class LowestCommonAncestor {
+
+    class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+
+}
