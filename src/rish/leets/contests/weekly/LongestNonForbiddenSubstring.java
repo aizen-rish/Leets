853bf82d9e54e194a308ec743e6ff89418971f2f
rish <kjeremyth@yandex.com>
2023-07-20 00:52:05 +0530

    problem4 --draft

---
+++ src/rish/leets/contests/weekly/LongestNonForbiddenSubstring.java
@@ -0,0 +1,22 @@
+package rish.leets.contests.weekly;
+
+/*
+ *            Weekly Contest 354
+ * 
+ * Problem #: 2781
+ * Problem link:https://leetcode.com/problems/length-of-the-longest-valid-substring/
+ * Contest link: https://leetcode.com/contest/weekly-contest-354/
+ * 
+ * Date Attempted: 28/06/2023
+ * 
+ * @author Rishabh Soni
+ * 
+ */
+public class LongestNonForbiddenSubstring {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
