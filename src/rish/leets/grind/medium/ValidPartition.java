45d3f2d78e7b703ef8a7612ca94a69380520e6c3
rish <kjeremyth@yandex.com>
2023-08-19 14:39:04 +0530

    add iterative approach for valid partition

--- src/rish/leets/grind/medium/ValidPartition.java
+++ src/rish/leets/grind/medium/ValidPartition.java
@@ -18,6 +18,9 @@ public class ValidPartition {
 
     static Map<Integer, Boolean> dp = new HashMap<>();
 
+    /*
+     * Recursive solution: Top-down approach
+     */
     public static boolean validPartition(int[] nums) {
         return validPartitionInternal(nums, nums.length - 1);
     }
@@ -59,11 +62,51 @@ public class ValidPartition {
         return ans;
     }
 
+    /*
+     * Iterative solution: Bottoms up appproach
+     * 
+     */
+    public static boolean validPartition2(int[] nums) {
+
+        int len = nums.length;
+        boolean[] dp2 = new boolean[len + 1];
+
+        dp2[0] = true;
+        dp2[1] = false;
+
+        for (int i = 1; i < len; i++) {
+
+            boolean ans = false;
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                ans |= dp2[i - 1];
+            }
+
+            if (i > 1) {
+
+                int a = nums[i];
+                int b = nums[i - 1];
+                int c = nums[i - 2];
+
+                if (a == b && b == c) {
+                    ans |= dp2[i - 2];
+                }
+                if (c == b - 1 && b == a - 1) {
+                    ans |= dp2[i - 2];
+                }
+            }
+
+            dp2[i + 1] = ans;
+        }
+
+        return dp2[len];
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 4, 4, 4, 5, 6 };
 
-        System.out.println(validPartition(arr));
+        System.out.println(validPartition2(arr));
     }
 
 }
