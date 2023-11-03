8b7646b6cb4ad82456b3d483001d4df81422d3e0
rish <19748474+aizen-rish@users.noreply.github.com>
2023-11-03 19:18:57 +0530

    adds code for nov2 soln

---
+++ src/rish/leets/grind/medium/CountNodesAverageOfSubTree.java
@@ -0,0 +1,53 @@
+package rish.leets.grind.medium;
+
+import rish.leets.grind.common.utils.TreeNode;
+
+public class CountNodesAverageOfSubTree {
+
+    class Node {
+        int sum;
+        int count;
+
+        public Node(int i, int j) {
+            this.sum = i;
+            this.count = j;
+        }
+    }
+
+    private int ans = 0;
+
+    public int averageOfSubtree(TreeNode root) {
+        avgInternal(root);
+        return ans;
+    }
+
+    private Node avgInternal(TreeNode node) {
+
+        if (node == null) {
+            return null;
+        }
+
+        Node left = avgInternal(node.left);
+        Node right = avgInternal(node.right);
+
+        int sum = node.val;
+        int count = 1;
+
+        if (left != null) {
+            sum += left.sum;
+            count += left.count;
+        }
+
+        if (right != null) {
+            sum += right.sum;
+            count += right.count;
+        }
+
+        if (sum / count == node.val) {
+            ans++;
+        }
+
+        return new Node(sum, count);
+    }
+
+}
